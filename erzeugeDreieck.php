487b646abfb5c832c556a4f72bfa6d7ed9884664
Fabian Bethge <sta1k3r@gmx.de>
2012-04-11 16:06:17 +0200

    Kommentar auf 'erzeugeDreieck.php'

--- erzeugeDreieck.php
+++ erzeugeDreieck.php
@@ -3,5 +3,6 @@ require_once "Dreieck.class.php"
 meinDreieck = new Dreieck;
 meinDreieck->berechUmfang(10, 10, 10);
 meinDreieck->berechGleichSeitig(4,4,3);
+//Was ist das eigentlich fürn Quark?`??
 
 ?>
